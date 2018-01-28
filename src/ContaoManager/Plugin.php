32994412f89e7d12106657c9eb8f61022b1f893b
Daniel Kiesel <icodr8@gmail.com>
2018-01-28 20:55:34 +0100

    optimized contao manager support

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,7 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Craffft\LogoBundle\ContaoManager;
 
-class Plugin
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Craffft\LogoBundle\CraffftLogoBundle;
+
+class Plugin implements BundlePluginInterface
 {
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(CraffftLogoBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setReplace(['logo']),
+        ];
+    }
 }
