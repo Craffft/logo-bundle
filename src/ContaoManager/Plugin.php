39676a8075274bdfc29f2b6c1ff53b0b14f57bcc
Daniel Kiesel <icodr8@gmail.com>
2018-01-28 22:44:59 +0100

    optimized contao manager support

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -8,10 +8,9 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Contao\ManagerPlugin\Dependency\DependentPluginInterface;
 use Craffft\LogoBundle\CraffftLogoBundle;
 
-class Plugin implements BundlePluginInterface, DependentPluginInterface
+class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
